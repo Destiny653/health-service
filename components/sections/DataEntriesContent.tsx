7608fd59ed80f3f1a07d9442ffcd1472b78dde20
Destiny653 <fokundem653@gmail.com>
2025-10-30 15:51:01 +0100

    dd

--- components/sections/DataEntriesContent.tsx
+++ components/sections/DataEntriesContent.tsx
@@ -488,7 +488,7 @@ export default function DataEntriesContent() {
       `}</style>
       
       {/* Header */}
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center p-4 bg-white border-b shadow-sm z-20 flex-shrink-0">
+      <div className="flex flex-col md:flex-row justify-between items-start md:items-center p-4 bg-white z-20 flex-shrink-0">
         <div className="flex items-center space-x-2 bg-blue-50 py-2 px-4 rounded-md border border-blue-200">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-blue-600" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
@@ -496,7 +496,7 @@ export default function DataEntriesContent() {
           <span className="text-blue-700">Bonaberi Health Center</span>
         </div>
 
-        <div className="flex items-center space-x-4 mt-4 md:mt-0 p-2 border rounded-lg bg-white shadow-sm">
+        <div className="flex items-center space-x-4 mt-4 md:mt-0 p-2 border rounded-md bg-white">
           <div className="flex items-center text-sm">
             <div className="w-3 h-3 rounded-full bg-red-500 mr-2"></div>
             No Submission (20%)
@@ -533,7 +533,7 @@ export default function DataEntriesContent() {
                     <span className="text-xs text-gray-600 whitespace-nowrap">Select Date:</span>
                     <Popover>
                       <PopoverTrigger asChild>
-                        <Button variant="outline" className="w-[240px] justify-start text-left font-normal h-8">
+                        <Button variant="outline" className="w-[240px] shadow-none justify-start text-left font-normal h-10">
                           <span className="mr-2">{format(selectedDate, 'PPP')}</span>
                         </Button>
                       </PopoverTrigger>
